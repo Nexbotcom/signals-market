7a84a1e3ecb04afbd8266f77f0473f06574bd391
Nexbotcom <chigerenzubechukwu@gmail.com>
2026-09-06 02:37:10 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Signals Market</h1>
+      <p>Coming soon.</p>
+    </div>
+  )
+}
