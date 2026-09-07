f467097a15a351537f50a117ee1ceadca5269de2
Nexbotcom <chigerenzubechukwu@gmail.com>
2026-09-07 02:35:08 +0100

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Signals Market</h1>
-      <p>Coming soon.</p>
-    </div>
-  )
-}
